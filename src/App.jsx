415e24238534dc9907699047e565689c0c4278cd
AlasgarovHaji <hacielesgerov0011@gmail.com>
2024-05-23 11:16:42 -0400

    "End of second commit"

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 
-import './App.css'
-import Footer from './components/layout/Footer/Footer'
-import Header from './components/layout/Header/Header'
-import Main from './components/layout/Main/Main'
+import Footer from './components/layout/Footer/Footer.jsx'
+import Header from './components/layout/Header/Header.jsx'
+import Main from './components/layout/Main/Main.jsx'
+import style from './App.module.jsx'
 
 function App() {
   
   return (
-    <>
+    <div className={style.top}>
       <Header/>
       <Main/>
       <Footer/>
-    </>
+    </div>
   )
 }
 
